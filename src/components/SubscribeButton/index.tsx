15f6d9ffa1fba725c301ce0f811cbd035e7e5b6a
RoMartins <rodrigomartins_santos1804@hotmail.com>
2022-02-28 11:00:46 -0300

    Config stripeCheckout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,26 @@
+import { useSession , signIn} from 'next-auth/react'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+  const {data: session} = useSession();
+
+  function HandleSubscribe() {
+    if(!session){
+      signIn('github')
+      return;
+    }
+
+
+  }
+
   return (
     <button 
       type="button"
       className={styles.subscribeButton}
+      onClick={HandleSubscribe}
     >
       Subscribe now 
     </button>
