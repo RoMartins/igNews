9dd3b45184f58000357f5d3717feabfe46dc58a1
RoMartins <rodrigomartins_santos1804@hotmail.com>
2022-03-13 00:12:34 -0300

    Header menu Links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import styles from './styles.module.scss'
 import {SignInButton} from '../SignInButton'
+import Link from 'next/link'
+
 
 export function Header() {
   return (
@@ -7,8 +9,13 @@ export function Header() {
       <div className={styles.headerContent}> 
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
+          <Link href='/'>
           <a className={styles.active}> Home </a>
+          </Link>
+
+          <Link href='/posts'>
           <a> Posts </a>
+          </Link>
         </nav>
         
         <SignInButton />
