5442559063382be7bc04d9e941e6cce61fc33980
RoMartins <rodrigomartins_santos1804@hotmail.com>
2022-03-12 23:37:07 -0300

    Create ListPosts

---
+++ src/services/primisc.ts
@@ -0,0 +1,14 @@
+import Prismic from '@prismicio/client'
+
+//const endpoint = Prismic.getRepositoryEndpoint()
+export function getPrismicClient() {
+  const prismic = Prismic.client(
+    process.env.PRISMIC_ENDPOINT,
+    {
+        
+      accessToken:     process.env.PRISMIC_ACCESS_TOKEN,
+    }
+  )
+
+  return prismic;
+}
